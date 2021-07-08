046a25ae85e344dcf95fd3e36e91d6273ff72726
space2pacman <space2pacman@gmail.com>
2021-07-09 02:17:56 +0300

    add post request api tasks add

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const Payload = require('./core/Payload');
 const tasks = require('./db/tasks');
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 app.get('/tasks', (request, response) => {
-		response.send(tasks);
+	response.send(tasks);
 });
 
 app.get('/tasks/:id', (request, response) => {
@@ -15,6 +19,18 @@ app.get('/tasks/:id', (request, response) => {
 
 	payload.add('task', task);
 	payload.add('message', message);
+
+	response.send(payload.get());
+})
+
+app.post('/tasks/add', (request, response) => {
+	const payload = new Payload();
+	const name = request.body.name;
+	const description = request.body.description;
+	
+	tasks.push({ id: 1, name, description });
+
+	payload.add('message', 'task has been added');
 
 	response.send(payload.get());
 })
